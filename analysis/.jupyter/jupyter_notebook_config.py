9925c31ab317dcd427028a53deac757d30d682ca
Sam Kritchevsky <sam.kritch@gmail.com>
2023-09-01 10:54:58 -0400

    wip

--- analysis/.jupyter/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-# Configuration file for ipython.
-
-# c = get_config()
-# Config for running in container
-c.NotebookApp.token = ''
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.open_browser = False
-c.NotebookApp.custom_display_url = 'http://localhost:8888/'
