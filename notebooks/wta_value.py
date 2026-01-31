57d1b5a933e4907dba20222c63135b6b6ad6a0f8
Sam Kritchevsky <sam.kritch@gmail.com>
2026-01-30 22:50:32 -0500

    fix wta notebook

--- notebooks/wta_value.py
+++ notebooks/wta_value.py
@@ -98,35 +98,19 @@ def _():
     mo.md(r"""
     Where to begin?
 
-
-    Observe that $\text{AV}(s)$ can be thought of as a "mean scaled electoral votes $\frac{e_s}{E}N$ per voter in the state":
-
-    $$
-    \begin{align}
-    \text{AV}(s) &= \left\langle\frac{e_{s(x)}}{E}N \right\rangle_{x \in s(x)} \\
-    &= \frac{N}{E}\frac{\sum_{x\in s} \frac{e_{s(x)}}{n_{s(x)}}}{\sum_{x \in s} 1} = \frac{n_s\frac{e_s}{n_s}}{n_s}\frac{N}{E} \\
-    &= \frac{e_s / E}{n_s / N}
-    \end{align}
-    $$
-
-
-    Now, what stands out about WTA districting is this: *for each state, only half of the votes + 1 ever affect the outcome of the national election*. Once the state has been won, the remaining votes (on both sides) count for nothing.
+    What stands out about WTA districting is this: *for each state, only half of the votes + 1 ever affect the outcome of the national election*. Once the state has been won, the remaining votes (on both sides) count for nothing.
 
     Furthermore, the half which *does* affect the election will have double the effect they would otherwise halve in terms of electors; each individual effectively moves the elector count by 2 units of $\text{AV}(s)$ rather than 1.
 
     Later, when we bring party affiliation into the mix, we'll be able to determine *which* votes had no effect; for now we only know it was some half.
 
-    So we'll consider a WTA state election to assign values to votes by a function **Winner-Takes-All W** or $\text{WW}(x)$:
-    - value $\text{WW}(x) = 2 \cdot \text{AV}(x) = 2\cdot \frac{e_{s(x)}/E}{n_{s(x)}/N}$ to $\approx \frac{n_{s(x)}}{2}$ members of the state
-    - value $\text{WW}(x) = 0$ to the remaining $\approx \frac{n_{s(x)}}{2}$ voters.
+    So we'll consider a WTA state election to assign values to votes by a function **Winner-Takes-All Value** or $\text{WTAV}(x)$:
+    - value $\text{WTAV}(x) = 2 \cdot \text{AV}(x) = 2\cdot \frac{e_{s(x)}/E}{n_{s(x)}/N}$ to $\approx \frac{n_{s(x)}}{2}$ members of the state
+    - value $\text{WTAV}(x) = 0$ to the remaining $\approx \frac{n_{s(x)}}{2}$ voters.
 
     We won't bother with the exact rounding of $\frac{n_{s(x)}}{2}$, and will ignore ties. For now, we won't think about which population $n_s$ is taken to measure; later we might want to use AP, VEP, or VP.
 
-
-    This assignment of values $V(x)$ will still have the same mean, $V(x) = \text{AV}(s(x)) = \left\langle\frac{e_{s(x)}}{E}N \right\rangle_{x \in s(x)}$.
-
-    But the inequality metrics MAD, RMS, and relative entropy _will_ be affected.
-
+    Note that this value has the same mean as AV. But it will not have the same MAD, Var, etc.
 
     ----
     """)
@@ -138,7 +122,7 @@ def _():
     mo.md(r"""
     # **M2:** Wasted Votes (w/o parties)
 
-    We'll call the inequality measure derived from $\text{EW}$ by the name "Wasted Votes" or WV. This is specifically the waste due to "districting" (a general term for the grouping of an electorate into any kind of hierarchical structure.). Later we will look at WV *with* party affiliation, at the L3 level, perhaps giving this another name.
+    We'll call the inequality measure derived from $\text{WTAV}$ by the name "Wasted Votes" or WV. This is specifically the waste due to "districting" (a general term for the grouping of an electorate into any kind of hierarchical structure.). Later we will look at WV *with* party affiliation, at the L3 level, perhaps giving this another name.
 
 
     What do these measures give for SW?
@@ -147,25 +131,29 @@ def _():
 
     $$
     \begin{align}
-    \text{MAD}[\text{EW}] &= \frac{1}{N}\sum_x \vert\text{EW(x)} - 1 \vert \\
-      &= \frac{1}{N}\sum_s \sum_{x \in s} \vert\text{EW(x)} - 1 \vert\\
+    \text{WV}_{\text{MAD}} &= \frac{1}{N}\sum_x \vert\text{WTAV(x)} - 1 \vert \\
+      &= \frac{1}{N}\sum_s \sum_{x \in s} \vert\text{WTAV(x)} - 1 \vert\\
       &= \frac{1}{N}\sum_s \left( \frac{n_s}{2}\vert 2\cdot \text{AV(s)} - 1\vert + \frac{n_s}{2}\vert -1\vert\right) \\
-      &= \frac{N}{2} +\frac{1}{N}\sum_s n_s \left\vert \text{AV(s)} - \frac{1}{2}\right\vert \\
+      &= \frac{1}{2} +\sum_s \frac{n_s}{N} \left\vert \text{AV(s)} - \frac{1}{2}\right\vert \\
     \end{align}
     $$
 
-    Note This expression is always $\ge 1$, and gives 1 when $\text{AV}(s)$ is uniformly equal to $1$ (or $0$, but we don't care about that).
+    If $\text{AV}(s) \ge \frac{1}{2}$ everywhere, the absolute value signs can be removed, and this expression comes reduces to $\frac{\sum n_s \text{AV}(s)}{N} = 1$.
 
     **M2.2** RMS Deviation:
 
     $$
     \begin{align}
-    \text{RMS}[\text{EW}] &= \sqrt{\frac{1}{N}\sum_x \left(\text{EW(x)} - 1 \right)^2} \\
-      &= \sqrt{\frac{1}{N}\sum_s \sum_{x \in s} {(\text{EW(x)} - 1 )}^2}\\
-      &= \sqrt{\frac{1}{N}\sum_s \left( \frac{n_s}{2}{(2 \cdot \text{AV(s)} - 1 )}^2 + \frac{n_s}{2}{(-1)}^2\right)}\\
-      &= \sqrt{\frac{N}{2} + \frac{1}{N}\sum_s n_s \left(\text{AV(s)} - \frac{1}{4} \right)^2}
+    \text{WV}_{\text{Var}} &= {\frac{1}{N}\sum_x \left(\text{WTAV(x)} - 1 \right)^2} \\
+      &= {\frac{1}{N}\sum_s \sum_{x \in s} {(\text{WTAV(x)} - 1 )}^2}\\
+      &= {\frac{1}{N}\sum_s \left( \frac{n_s}{2}{(2 \cdot \text{AV(s)} - 1 )}^2 + \frac{n_s}{2}{(-1)}^2\right)}\\
+      &= \sum_s \frac{n_s}{N}\left(2 \cdot (\text{AV(s)}) ^2 - 2 \text{AV(s)}  +1\right)\\
+      &= \left(2\sum_s \frac{n_s}{N}\cdot (\text{AV(s)}) ^2 \right) - 1\\
     \end{align}
     $$
+
+    If $\text{AV}(s) = 1$ this expression is exactly 1.
+
 
 
     **M2.3** Relative Entropy:
@@ -173,11 +161,16 @@ def _():
 
     $$
     \begin{align}
-    H\left[\frac{\text{EW}(X)}{N} ~\Vert~ \frac{1}{N}\right] &= \sum_x \frac{\text{EW}(x)}{N} \log \frac{\text{EW}(x)/N}{1 / N} \\
+    \text{WV}_{\text{Ent}} = H\left[\frac{\text{WTAV}(X)}{N} ~\Vert~ \frac{1}{N}\right] &= \sum_x \frac{\text{WTAV}(x)}{N} \log \frac{\text{WTAV}(x)/N}{1 / N} \\
       &= \frac{1}{N}\sum_x  \text{AV}(x) \cdot \log \text{AV}(x) \\
-      &= \frac{1}{N}\sum_s n_s \cdot \text{AV}(s) \cdot \log \text{AV}(s)
+      &= \frac{1}{N}\sum_s \left( \frac{n_s}{2} \cdot 2 \cdot  \text{AV}(s) \cdot( \log 2 \cdot \text{AV}(s)) + \frac{n_s}{2} \cdot 0
+      \right)\\
+      &=\sum_s   \frac{n_s}{N} \cdot \text{AV}(s) \cdot \left(\log \text{AV}(s) + \log 2\right) \\
+      &=  H\left[\frac{\text{AV}(X)}{N} ~\Vert~ \frac{1}{N}\right] + \log 2
     \end{align}
     $$
+
+    Using WTAV simply adds $\log 2$ to the entropy over AV, and if the base of the log is 2 then this, too, adds exactly 1.
     """)
     return
 
@@ -185,12 +178,13 @@ def _():
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    TODO: all of these = 1 if AV is uniform... this means they really aren't doing what I want.
+    ---
 
-    That, also, is exactly what these give for a national general election. They might be capturing some inequality in the elector counts, but...
+    All of these expression give exactly 1 for uniform $\text{AV}(s) = 1$, i.e. for "perfectly proportional apportionment". So:" considering a WTA election automatically adds 1 to its MAD/Var/Entropy, but this is *completely insensitive to the size of the states*. As long as every voter is in *some* WTA district, their vote counts 0 half the time.
 
+    Note that we are *not* considering the WTA nature of the national election. At the "top" level, every vote has an influence on the candidates, so we don't need to account for this.
 
-    TODO: half the votes are always "wasted" in a national election. Can we subtract this off any of the above?
+    Still: these metrics aren't much use. These will be a handy baseline for when we consider wasted votes *with* parties, but to capture the effect of WTA elections in the states we need some other measure which distinguishes large states from small ones. Intuitively it should be "more fair" to divide a state up into districts than to consider it all as one block--there are more opportunity to have marginal effects on the outermost election. We'll turn our attention to this in the next analysis.
     """)
     return
 
